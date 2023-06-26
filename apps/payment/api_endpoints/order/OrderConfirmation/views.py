93b67a831c48c98b7c2df21bcc01bccb76594438
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-06-26 13:30:35 +0500

    feat: some changes

--- apps/payment/api_endpoints/order/OrderConfirmation/views.py
+++ apps/payment/api_endpoints/order/OrderConfirmation/views.py
@@ -4,6 +4,7 @@ from rest_framework import response, views
 class ConfirmAPIView(views.APIView):
     def post(self, request, *args, **kwargs):
         print("Request is POST:", request.data)
+        print("Response is POST:", request)
         return response.Response({"message": "Ok"}, status=200)
 
     def get(self, request, *args, **kwargs):
